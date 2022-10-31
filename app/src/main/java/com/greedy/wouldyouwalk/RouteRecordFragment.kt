66e6a6d05893489899c2ec9e807e5222264539a6
송다인 <inhere31@gmail.com>
2022-10-31 14:43:11 +0900

    송다인 googleMap Btnversion1

--- app/src/main/java/com/greedy/wouldyouwalk/RouteRecordFragment.kt
+++ app/src/main/java/com/greedy/wouldyouwalk/RouteRecordFragment.kt
@@ -2,25 +2,14 @@ package com.greedy.wouldyouwalk
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Context.LOCATION_SERVICE
 import android.graphics.Color
-import android.location.Location
-import android.location.LocationManager
-import android.location.LocationRequest
 import android.os.Bundle
-import android.os.Looper
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.FragmentManager
-import com.google.android.gms.location.*
 import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.*
 import com.greedy.wouldyouwalk.databinding.FragmentRouterecordBinding
 
 class RouteRecordFragment : Fragment(), OnMapReadyCallback {
@@ -42,12 +31,12 @@ class RouteRecordFragment : Fragment(), OnMapReadyCallback {
 
         binding = FragmentRouterecordBinding.inflate(inflater, container, false)
 
+        val googleMap: GoogleMap
 
         /* 화면에 지도 띄우기 */
         mView = binding.mapview as MapView
         mView.onCreate(savedInstanceState)
         mView.getMapAsync(this)
-
 
         return binding.root
     }
@@ -58,15 +47,79 @@ class RouteRecordFragment : Fragment(), OnMapReadyCallback {
         /* 현재 위치로 이동 설정(애뮬레이터는 위치 받을 수 없어서 구글 본사로 이동됨) */
         googleMap.isMyLocationEnabled = true
 
-        /* 디폴트 위치 설정 */
+        /* 디폴트 위치 */
         val defaultMarker = LatLng(37.571919, 126.987316)
-        googleMap.addMarker(MarkerOptions().position(defaultMarker).title("하이미디어컴퓨터학원 종로 캠퍼스"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultMarker, 15f))
 
-    }
+        /* 인사동길-열린녹지광장 마커+경로 */
+        binding.walk1.setOnClickListener {
 
-    private fun onMarkerClick() {
-        Log.d("마커이벤트", "동작확인!")
+            val walk1s = LatLng(37.572148, 126.986763)
+            googleMap.addMarker(MarkerOptions().position(walk1s).title("📍 인사동길")
+                     .snippet("인사동길 탐방 고고🧐").icon(BitmapDescriptorFactory.fromResource(R.drawable.mapicon2)))
+
+            val walk1a = LatLng(37.575646, 126.982810)
+            googleMap.addMarker(MarkerOptions().position(walk1a).title("📍 열린녹지광장")
+                .snippet("열린녹지광장에서 힐링! 🌻").icon(BitmapDescriptorFactory.fromResource(R.drawable.mapicon3)))
+
+            val walk1 = LatLng(37.574240, 126.984535)
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(walk1, 16f))
+
+            val polylineOption = PolylineOptions().color(Color.argb(117, 59, 100, 1))
+                .add(LatLng(37.572630, 126.986485))
+                .add(LatLng(37.572992, 126.986202))
+                .add(LatLng(37.573398, 126.985760))
+                .add(LatLng(37.575313, 126.983365))
+                .add(LatLng(37.575513, 126.982920))
+                .add(LatLng(37.575646, 126.982810))
+
+            val polylineOptions = googleMap.addPolyline(polylineOption)
+
+        }
+
+        /* 북촌 한옥마을 마커+경로 */
+        binding.walk2.setOnClickListener {
+            val walk2s = LatLng(37.581434, 126.985007)
+            googleMap.addMarker(MarkerOptions().position(walk2s).title("📍 북촌한옥마을")
+                .snippet("한복 입고 산책은 어때? ✨").icon(BitmapDescriptorFactory.fromResource(R.drawable.mapicon4)))
+            val walk2a = LatLng(37.582320, 126.983560)
+            googleMap.addMarker(MarkerOptions().position(walk2a).title("📍 북촌한옥마을")
+                .snippet("고즈넉한 한옥을 구경할 수 있어~🍀").icon(BitmapDescriptorFactory.fromResource(R.drawable.mapicon5)))
+
+            val walk2 = LatLng(37.581906, 126.984023)
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(walk2, 17f))
+
+            val polylineOption = PolylineOptions().color(Color.argb(117, 59, 100, 1))
+                .add(LatLng(37.581434, 126.985007))
+                .add(LatLng(37.581454, 126.984661))
+                .add(LatLng(37.581648, 126.984484))
+                .add(LatLng(37.581753, 126.984423))
+                .add(LatLng(37.581993, 126.983747))
+                .add(LatLng(37.582183, 126.983584))
+                .add(LatLng(37.582320, 126.983560))
+
+            val polylineOptions = googleMap.addPolyline(polylineOption)
+        }
+
+        /* 덕수궁 돌담길 마커+경로 */
+        binding.walk3.setOnClickListener {
+            val walk3s = LatLng(37.564844, 126.975005)
+            googleMap.addMarker(MarkerOptions().position(walk3s).title("📍 덕수궁돌담길")
+                .snippet("커플 환영!!!!!!!!!ㅋ").icon(BitmapDescriptorFactory.fromResource(R.drawable.mapicon6)))
+
+            val walk3 = LatLng(37.565029, 126.974926)
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(walk3, 16f))
+
+            val polylineOption = PolylineOptions().color(Color.argb(117, 59, 100, 1))
+                .add(LatLng(37.564854, 126.976649))
+                .add(LatLng(37.564944, 126.975918))
+                .add(LatLng(37.564757, 126.974066))
+                .add(LatLng(37.565225, 126.973548))
+
+            val polylineOptions = googleMap.addPolyline(polylineOption)
+
+        }
+
     }
 
     override fun onStart() {
